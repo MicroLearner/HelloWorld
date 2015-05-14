dd6ffae64a0ba449a901c6c5543f465c9aa8fdf5
MicroCai <microcai@foxmail.com>
2015-05-14 23:14:45 +0800

    dfsdfsdfsdfsdfsdf

---
+++ AVDemo/AVDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVDemo
+//
+//  Created by MicroCai on 15/5/8.
+//  Copyright (c) 2015年 MicroCai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
